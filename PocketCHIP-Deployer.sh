535dfd17f990f4bf43266ea2a034f4f9299c71c6
Fretzy <fretzy@gmail.com>
2022-01-04 16:44:06 -0500

    Update PocketCHIP-Deployer.sh

--- PocketCHIP-Deployer.sh
+++ PocketCHIP-Deployer.sh
@@ -4,62 +4,63 @@
   tput setaf 1;   echo "   #  #  #"
   tput setaf 1;   echo "  #########"
   tput setaf 3;   echo "###       ###"
-  tput setaf 2;   echo "  # {#}   #"
+  tput setaf 3;   echo "  # {#}   #"
   tput setaf 2;   echo "###  '\######"
-  tput setaf 4;   echo "  #       #"
+  tput setaf 2;   echo "  #       #"
   tput setaf 6;   echo "###       ###"
-  tput setaf 5;   echo "  ########"
+  tput setaf 6;   echo "  ########"
   tput setaf 5;   echo "   #  #  #"
-  tput setaf 7;   echo -------------
+  tput setaf 5;   echo -------------
   echo ""
   
-  
   tput setaf 1;   echo "Updating /etc/apt/sources.list"
-  tput setaf 7;   echo ""
+  echo ""
 sed -e '/jessie-backports/s/^#*/#/g' -i /etc/apt/sources.list
 sed -i 's/ftp.us/deb/' /etc/apt/sources.list
-sed -i 's/opensource.nextthing.co/chip.jfpossibilities.com/' /etc/apt/sources.list
-  tput setaf 1;   echo "Done."
+sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/sources.list
+  echo "Done."
+  read -rsp $'Press any key to continue...\n' -n1 key
   
+  echo ""
+  tput setaf 3;   echo "Updating /etc/apt/preferences"
+  echo ""
+sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/preferences
+  echo "Done."
+  read -rsp $'Press any key to continue...\n' -n1 key
   
-  tput setaf 7;   echo ""
-  tput setaf 2;   echo "Updating /etc/apt/preferences"
-  tput setaf 7;   echo ""
-sed -i 's/opensource.nextthing.co/chip.jfpossibilities.com/' /etc/apt/preferences
-  tput setaf 2;   echo "Done."
-  
-  
-  tput setaf 7;   echo ""
-  tput setaf 3;   echo "Updating APT package list"
-  tput setaf 7;   echo ""
+  echo ""
+  tput setaf 2;   echo "Updating APT package list"
+  echo ""
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
 apt update
-  tput setaf 3;   echo "Done."
+  echo "Done."
+  read -rsp $'Press any key to continue...\n' -n1 key
   
-  tput setaf 7;   echo ""
-  tput setaf 3;   echo "Updating Pico-8"
-  tput setaf 7;   echo ""
+  echo ""
+  tput setaf 6;   echo "Updating Pico-8"
+  echo ""
 rm -rf /usr/lib/pico8
 wget www.lexaloffle.com/dl/chip/pico-8_0.2.4_chip.zip
 unzip pico-8_0.2.4_chip.zip -d /usr/lib
 mv /usr/lib/pico-8 /usr/lib/pico8
 rm pico-8_0.2.4_chip.zip
-  tput setaf 3;   echo "Done."
+  echo "Done."
+  read -rsp $'Press any key to continue...\n' -n1 key
   
-  tput setaf 7;   echo ""
-  tput setaf 4;   echo "Installing OpenSSH Server"
-  tput setaf 7;   echo ""
+  echo ""
+  tput setaf 5;   echo "Installing OpenSSH Server"
+  echo ""
 apt -y install openssh-server
-  tput setaf 4;   echo "Done."
+  echo "Done."
+  read -rsp $'Press any key to continue...\n' -n1 key
   
-  tput setaf 7;   echo ""
+  echo ""
   tput setaf 5;   echo "Starting SSH Server"
-  tput setaf 7;   echo ""  
+  echo ""  
 systemctl start ssh
-  tput setaf 4;   echo "Done."
+echo "Done."
   
   tput setaf 7;   echo ""
-  tput setaf 7;   echo "  Setup Complete! "
-  tput setaf 6;   echo "CHIP's IP Address:"
-  tput setaf 7;   echo ""  
+  echo "  Setup Complete! "
+  echo "CHIP's IP Address:"
 ip -4 a show wlan0 | grep -Po 'inet \K[0-9.]*'
