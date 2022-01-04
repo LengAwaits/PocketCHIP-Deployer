a87d61ee4450283298536506bbeb31f49332e9c5
Fretzy <fretzy@gmail.com>
2022-01-04 17:08:02 -0500

    Update PocketCHIP-Deployer.sh

--- PocketCHIP-Deployer.sh
+++ PocketCHIP-Deployer.sh
@@ -15,6 +15,7 @@
   
   tput setaf 1;   echo "Updating /etc/apt/sources.list"
   echo ""
+  sleep 5
 sed -e '/jessie-backports/s/^#*/#/g' -i /etc/apt/sources.list
 sed -i 's/ftp.us/deb/' /etc/apt/sources.list
 sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/sources.list
@@ -24,6 +25,7 @@ sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/sources.list
   echo ""
   tput setaf 3;   echo "Updating /etc/apt/preferences"
   echo ""
+  sleep 5
 sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/preferences
   echo "Done."
   read -rsp $'Press any key to continue...\n' -n1 key
@@ -31,14 +33,16 @@ sed -i 's/opensource.nextthing.co/chip.rodeo/' /etc/apt/preferences
   echo ""
   tput setaf 2;   echo "Updating APT package list"
   echo ""
+  sleep 5
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
 apt update
-  echo "Done."
+  tput setaf 2;   echo "Done."
   read -rsp $'Press any key to continue...\n' -n1 key
   
   echo ""
   tput setaf 6;   echo "Updating Pico-8"
   echo ""
+  sleep 5
 rm -rf /usr/lib/pico8
 wget www.lexaloffle.com/dl/chip/pico-8_0.2.4_chip.zip
 unzip pico-8_0.2.4_chip.zip -d /usr/lib
@@ -50,13 +54,15 @@ rm pico-8_0.2.4_chip.zip
   echo ""
   tput setaf 5;   echo "Installing OpenSSH Server"
   echo ""
+  sleep 5
 apt -y install openssh-server
-  echo "Done."
+  tput setaf 5;   echo "Done."
   read -rsp $'Press any key to continue...\n' -n1 key
   
   echo ""
   tput setaf 5;   echo "Starting SSH Server"
   echo ""  
+  sleep 5
 systemctl start ssh
 echo "Done."
   
